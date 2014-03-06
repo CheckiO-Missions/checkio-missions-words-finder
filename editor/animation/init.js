704e4874926d69e4d9815f04fffc0313d5e36691
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-03-07 11:51:25 +1200

    fix explanation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -40,7 +40,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
 
             var checkioInput = data.in;
-            var checkioInputStr = JSON.stringify(checkioInput).replace(/\[/g, "(").replace(/\]/g, ")");
+            var checkioInputStr = JSON.stringify(checkioInput[0]) + ", " + JSON.stringify(checkioInput[1]);
 
             if (data.error) {
                 $content.find('.call').html('Fail: checkio(' + JSON.stringify(checkioInput) + ')');
